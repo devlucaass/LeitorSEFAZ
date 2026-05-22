8c23dcef96f937b06406eab9702d17e0557127bb
devlucaass <lucasvinicius3719@gmail.com>
2026-05-22 19:35:43 -0300

    refactor: melhora na organização do código

--- src/services/excel_service.py
+++ src/services/excel_service.py
@@ -1,27 +1,39 @@
+import os
 import pyautogui
 import pandas as pd
+from tkinter import messagebox
 from utils.funcs import validar_planilha
 
 class ExcelBot:
 
-    # Inicializa a classe, recebendo a chave como parâmetro para garantir o carregamento da Nota Fiscal.
     def __init__(self, chave):
-        self.dados = pd.read_csv(f'data/notas_fiscais/nf_{chave[:44]}.csv')
-    
-    # Garante que esteja na célula correta para iniciar a inserção dos dados.
+        self.caminho_csv = f'data/notas_fiscais/nf_{chave[:44]}.csv'
+
+        if not os.path.exists(self.caminho_csv):
+            messagebox.showerror('Erro', 'Arquivo CSV não encontrado. Certifique-se de que a coleta de dados foi realizada com sucesso.')
+            raise FileNotFoundError(f'Arquivo {self.caminho_csv} não encontrado.')
+
+        self.dados = pd.read_csv(self.caminho_csv)
+
     def celula_inicial(self, celula):
         pyautogui.hotkey('alt', 'tab')
         pyautogui.hotkey('ctrl', 'g')
         pyautogui.write(celula)
         pyautogui.press('enter')
-    
-    # Insere os dados na planilha, iteirando sobre cada linha da Nota Fiscal extraída.
+
     def inserir_no_excel(self, planilha_buscada):
-        planilha_atual = validar_planilha(planilha_buscada)
-        if planilha_atual:
-            for linha in self.dados.values:
-                for valor in linha:
+        try:
+            planilha_atual = validar_planilha(planilha_buscada)
+
+            if not planilha_atual:
+                return
+
+            for dados_linha in self.dados.values:
+                for valor in dados_linha:
                     pyautogui.write(str(valor))
                     pyautogui.press('tab')
 
                 pyautogui.press('enter')
+
+        except Exception:
+            messagebox.showerror('Erro', 'Ocorreu um erro ao inserir os dados no Excel.')
