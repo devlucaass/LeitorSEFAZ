b6c22a68355be90a257a2ac230628c1f305365d4
devlucaass <lucasvinicius3719@gmail.com>
2026-06-06 14:15:41 -0300

    feat/refactor: melhoria na validação e tratamento de erros

--- src/services/sefaz_service.py
+++ src/services/sefaz_service.py
@@ -13,6 +13,13 @@ class SefazBot:
         self.driver = None
         self.chave = chave
 
+    def fechar_navegador(self):
+        if self.driver:
+            self.driver.quit()
+
+    def converter_numero(self, valor):
+        return float(valor.replace('.', '').replace(',', '.'))
+
     def configura_navegador(self, site, headless=False):
 
         try:
@@ -28,6 +35,9 @@ class SefazBot:
             self.driver.get(site)
 
         except Exception as erro:
+
+            self.fechar_navegador()
+
             messagebox.showerror('Erro', f'Ocorreu um erro ao iniciar navegador:\n{erro}')
 
     def digita_chave(self):
@@ -35,124 +45,73 @@ class SefazBot:
         try:
             WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'chave'))).send_keys(self.chave)
 
-            iframe = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//iframe[contains(@title,'reCAPTCHA')]")))
-
+            iframe = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//iframe[contains(@title,'reCAPTCHA')]")))
             self.driver.switch_to.frame(iframe)
 
-            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "recaptcha-anchor"))).click()
-
-            WebDriverWait(self.driver, 600).until(lambda d:d.find_element(By.ID,"recaptcha-anchor").get_attribute("aria-checked") == "true")
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor'))).click()
+            WebDriverWait(self.driver, 600).until(lambda d:d.find_element(By.ID,'recaptcha-anchor').get_attribute('aria-checked') == 'true')
 
             self.driver.switch_to.default_content()
 
-            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(text(), 'Consultar')]"))).click()
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Consultar')]"))).click()
 
         except Exception as erro:
-            messagebox.showerror('Erro', f'Ocorreu um erro:\n{erro}')
-
-            self.driver.quit()
+            self.fechar_navegador()
+            messagebox.showerror(f'Ocorreu um erro:\n{erro}')
 
     def coleta_dados(self):
 
         try:
             WebDriverWait(self.driver, 600, poll_frequency=1).until(EC.presence_of_element_located((By.ID, 'tabResult')))
-
-            WebDriverWait(self.driver, 60, poll_frequency=1 ).until(EC.presence_of_all_elements_located( (By.CLASS_NAME, 'txtTit')))
+            WebDriverWait(self.driver, 60, poll_frequency=1).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'txtTit')))
 
             linhas = self.driver.find_elements(By.CSS_SELECTOR,'#tabResult tbody tr')
 
+            data = self.driver.find_element(By.XPATH, '//strong[contains(text(), "Data de Emissão")]/parent::li').text
+            data = (data.split('Data de Emissão:')[-1].split('\n')[0].split(' - ')[0].strip().split(' ')[0])
+
+            estabelecimento = self.driver.find_element(By.ID, 'u20').text
+
             dados = []
 
-            data = self.driver.find_element(By.XPATH,'//strong[contains(text(), "Data de Emissão")]/parent::li').text
-
-            data = (data.split("Data de Emissão:")[-1].split("\n")[0].split(" - ")[0].strip().split(" ")[0])
-
-            estabelecimento = self.driver.find_element(By.ID,'u20').text
-
             for linha in linhas:
-                spans = linha.find_elements(By.CLASS_NAME,'txtTit')
+                if not linha.find_elements(By.CLASS_NAME, 'txtTit'):
+                    continue
 
-                if spans:
-                    produto = linha.find_element(By.CLASS_NAME,'txtTit').text
+                produto = linha.find_element(By.CLASS_NAME, 'txtTit').text
 
-                    quantidade = linha.find_element(By.CLASS_NAME,'Rqtd').text
+                quantidade = linha.find_element(By.CLASS_NAME, 'Rqtd').text
+                quantidade = quantidade.replace('Qtde.:', '').strip()
+                quantidade = self.converter_numero(quantidade)
 
-                    quantidade = (quantidade.replace('Qtde.:', '').strip())
+                grandeza = linha.find_element(By.CLASS_NAME, 'RUN').text
+                grandeza = grandeza.replace('UN:', '').strip()
 
-                    quantidade = float(quantidade.replace('.', '').replace(',', '.'))
+                valor_unitario = linha.find_element(By.CLASS_NAME, 'RvlUnit').text
+                valor_unitario = (valor_unitario.split(':')[-1].strip())
+                valor_unitario = self.converter_numero(valor_unitario)
 
-                    grandeza = linha.find_element(
-                        By.CLASS_NAME,
-                        'RUN'
-                    ).text
+                valor_total = linha.find_element(By.XPATH, './td[2]').text
+                valor_total = (valor_total.split('\n')[-1].strip())
+                valor_total = self.converter_numero(valor_total)
 
-                    grandeza = (
-                        grandeza
-                        .replace('UN:', '')
-                        .strip()
-                    )
-
-                    valor_unitario = linha.find_element(
-                        By.CLASS_NAME,
-                        'RvlUnit'
-                    ).text
-
-                    valor_unitario = (
-                        valor_unitario
-                        .split(':')[-1]
-                        .strip()
-                    )
-
-                    valor_unitario = float(
-                        valor_unitario
-                        .replace('.', '')
-                        .replace(',', '.')
-                    )
-
-                    valor_total = linha.find_element(
-                        By.XPATH,
-                        './td[2]'
-                    ).text
-
-                    valor_total = (
-                        valor_total
-                        .split('\n')[-1]
-                        .strip()
-                    )
-
-                    valor_total = float(
-                        valor_total
-                        .replace('.', '')
-                        .replace(',', '.')
-                    )
-
-                    dados.append({
-                        'data': data,
-                        'estabelecimento': estabelecimento,
-                        'produto': produto,
-                        'grandeza': grandeza,
-                        'quantidade_comprada': '-',
-                        'valor_unitario': valor_unitario,
-                        'quantidade': quantidade,
-                        'valor_total': valor_total
-                    })
+                dados.append({
+                    'data': data,
+                    'estabelecimento': estabelecimento,
+                    'produto': produto,
+                    'grandeza': grandeza,
+                    'quantidade_comprada': '-',
+                    'valor_unitario': valor_unitario,
+                    'quantidade': quantidade,
+                    'valor_total': valor_total
+                })
 
             return dados
 
         except Exception as erro:
+            messagebox.showerror('Erro', f'Ocorreu um erro:\n{erro}')
 
-            messagebox.showerror(
-                'Erro',
-                f'Ocorreu um erro:\n{erro}'
-            )
+            return []
 
         finally:
-            self.driver.quit()
-
-if __name__ == "__main__":
-    sefaz = SefazBot('26260205677591002950651130001034101359159580')
-    sefaz.configura_navegador('https://nfce.sefaz.pe.gov.br:444/nfce/consulta', headless=False)
-    sefaz.digita_chave()
-    print(sefaz.coleta_dados())
-
-    
+            self.fechar_navegador()
