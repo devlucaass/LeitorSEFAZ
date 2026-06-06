d1d0e396e83bbd18d54530a203fb62635f033b27
devlucaass <lucasvinicius3719@gmail.com>
2026-06-06 10:53:14 -0300

    fix: resolve bug com o valor virar texto
    - Resolve o problema do Excel entender número como texto.
    - Trata os números e os converte em float antes de exportar para o Excel, garantindo que sejam reconhecidos como números.

--- src/services/sefaz_service.py
+++ src/services/sefaz_service.py
@@ -1,24 +1,20 @@
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from tkinter import messagebox
-from utils.funcs import criar_pasta
 
 
 class SefazBot:
 
-    # Inicializa o bot e cria a pasta de saída para as NFs
     def __init__(self, chave):
-        criar_pasta('data/notas_fiscais')
-
         self.driver = None
         self.chave = chave
 
-    # Configura e inicia o navegador
     def configura_navegador(self, site, headless=False):
+
         try:
             options = Options()
             options.set_preference('dom.webnotifications.enabled', False)
@@ -29,87 +25,134 @@ class SefazBot:
 
             self.driver = webdriver.Firefox(options=options)
             self.driver.maximize_window()
-        
             self.driver.get(site)
 
-        except Exception:
-            messagebox.showerror('Erro', f'Ocorreu um erro inesperado. Fechando navegador...')
-            self.driver.quit()
+        except Exception as erro:
+            messagebox.showerror('Erro', f'Ocorreu um erro ao iniciar navegador:\n{erro}')
 
-    # Preenche o campo da chave e aguarda o CAPTCHA ser resolvido
     def digita_chave(self):
+
         try:
-            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID,'chave'))).send_keys(self.chave)
-            
-            iframe = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//iframe[contains(@title,'reCAPTCHA')]")))
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'chave'))).send_keys(self.chave)
+
+            iframe = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//iframe[contains(@title,'reCAPTCHA')]")))
+
             self.driver.switch_to.frame(iframe)
 
             WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "recaptcha-anchor"))).click()
-            
-            WebDriverWait(self.driver, 600).until(lambda d: d.find_element(By.ID, "recaptcha-anchor").get_attribute("aria-checked") == "true")
+
+            WebDriverWait(self.driver, 600).until(lambda d:d.find_element(By.ID,"recaptcha-anchor").get_attribute("aria-checked") == "true")
 
             self.driver.switch_to.default_content()
 
-            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Consultar')]"))).click()
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(text(), 'Consultar')]"))).click()
 
-        except Exception:
-            messagebox.showerror('Erro', f'Ocorreu um erro inesperado. Fechando navegador...')
+        except Exception as erro:
+            messagebox.showerror('Erro', f'Ocorreu um erro:\n{erro}')
+
             self.driver.quit()
 
-    # Faz uma varredura no site (WebScrapping), buscando as informações de compra.
     def coleta_dados(self):
-        try:
-            # Aguarda o máximo de tempo para o usuário resolver o CAPTCHA
-            WebDriverWait(self.driver, 600, poll_frequency=1).until(EC.presence_of_element_located((By.ID,'tabResult')))
 
-            # Aguarda carregamento da tabela 
-            WebDriverWait(self.driver, 60, poll_frequency=1).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'txtTit')))
+        try:
+            WebDriverWait(self.driver, 600, poll_frequency=1).until(EC.presence_of_element_located((By.ID, 'tabResult')))
+
+            WebDriverWait(self.driver, 60, poll_frequency=1 ).until(EC.presence_of_all_elements_located( (By.CLASS_NAME, 'txtTit')))
 
             linhas = self.driver.find_elements(By.CSS_SELECTOR,'#tabResult tbody tr')
 
             dados = []
 
-            data = self.driver.find_element(By.XPATH, '//strong[contains(text(), "Data de Emissão")]/parent::li').text
-            data = data.split("Data de Emissão:")[-1].split("\n")[0].split(" - ")[0].strip().split(" ")[0]
+            data = self.driver.find_element(By.XPATH,'//strong[contains(text(), "Data de Emissão")]/parent::li').text
 
-            estabelecimento = self.driver.find_element(By.ID, 'u20').text
+            data = (data.split("Data de Emissão:")[-1].split("\n")[0].split(" - ")[0].strip().split(" ")[0])
+
+            estabelecimento = self.driver.find_element(By.ID,'u20').text
 
             for linha in linhas:
                 spans = linha.find_elements(By.CLASS_NAME,'txtTit')
-        
-                if spans:                
-                
+
+                if spans:
                     produto = linha.find_element(By.CLASS_NAME,'txtTit').text
 
-                    quantidade = linha.find_element(By.CLASS_NAME,'Rqtd').text.replace('Qtde.:','').strip()
-                    quantidade = quantidade.replace(".", ",")
+                    quantidade = linha.find_element(By.CLASS_NAME,'Rqtd').text
 
-                    grandeza = linha.find_element(By.CLASS_NAME,'RUN').text.replace('UN:','').strip()
+                    quantidade = (quantidade.replace('Qtde.:', '').strip())
 
-                    valor_unitario = linha.find_element(By.CLASS_NAME,'RvlUnit').text
-                    valor_unitario = valor_unitario.split(":")[-1].strip().replace(".", ",")
+                    quantidade = float(quantidade.replace('.', '').replace(',', '.'))
 
-                    valor_total = linha.find_element(By.XPATH,'./td[2]').text
-                    valor_total = valor_total.split("\n")[-1].replace(".", ",")
+                    grandeza = linha.find_element(
+                        By.CLASS_NAME,
+                        'RUN'
+                    ).text
+
+                    grandeza = (
+                        grandeza
+                        .replace('UN:', '')
+                        .strip()
+                    )
+
+                    valor_unitario = linha.find_element(
+                        By.CLASS_NAME,
+                        'RvlUnit'
+                    ).text
+
+                    valor_unitario = (
+                        valor_unitario
+                        .split(':')[-1]
+                        .strip()
+                    )
+
+                    valor_unitario = float(
+                        valor_unitario
+                        .replace('.', '')
+                        .replace(',', '.')
+                    )
+
+                    valor_total = linha.find_element(
+                        By.XPATH,
+                        './td[2]'
+                    ).text
+
+                    valor_total = (
+                        valor_total
+                        .split('\n')[-1]
+                        .strip()
+                    )
+
+                    valor_total = float(
+                        valor_total
+                        .replace('.', '')
+                        .replace(',', '.')
+                    )
 
                     dados.append({
-                        "data": data,
-                        "estabelecimento": estabelecimento,
-                        "produto": produto,
-                        "grandeza": grandeza,
-                        "quantidade_comprada": '-',
-                        "valor_unitario": str(valor_unitario),
-                        "quantidade": str(quantidade),
-                        "valor_total": str(valor_total)
+                        'data': data,
+                        'estabelecimento': estabelecimento,
+                        'produto': produto,
+                        'grandeza': grandeza,
+                        'quantidade_comprada': '-',
+                        'valor_unitario': valor_unitario,
+                        'quantidade': quantidade,
+                        'valor_total': valor_total
                     })
 
-            df = pd.DataFrame(dados)
-            df.to_csv(f"data/notas_fiscais/nf_{self.chave[:44]}.csv",index=False,encoding="utf-8")
+            return dados
 
-        except Exception:
-            messagebox.showerror('Erro', f'Ocorreu um erro inesperado. Fechando navegador...')
+        except Exception as erro:
+
+            messagebox.showerror(
+                'Erro',
+                f'Ocorreu um erro:\n{erro}'
+            )
 
         finally:
             self.driver.quit()
 
-        return self.chave[:44]
+if __name__ == "__main__":
+    sefaz = SefazBot('26260205677591002950651130001034101359159580')
+    sefaz.configura_navegador('https://nfce.sefaz.pe.gov.br:444/nfce/consulta', headless=False)
+    sefaz.digita_chave()
+    print(sefaz.coleta_dados())
+
+    
