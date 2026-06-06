ce7f63f4af8e1a700fe860238c7c0b5952862073
devlucaass <lucasvinicius3719@gmail.com>
2026-06-06 16:21:35 -0300

    refactor: remoção do código morto após a migração do openpyxl

--- src/utils/funcs.py
+++ src/utils/funcs.py
@@ -1,61 +1,8 @@
-import os
 import cv2
-import pyautogui
-import pyperclip
 from pyzbar import pyzbar
 from openpyxl import Workbook
 from utils.validators import valida_chave
 from tkinter import messagebox, filedialog
-from config.config_manager import load_config
-
-config = load_config()
-
-pyautogui.PAUSE = config['sleep']
-
-# Copia o nome de planilha em foco
-def copiar_foco():
-    pyautogui.press('alt')
-    pyautogui.press('c')
-    pyautogui.press('o')
-    pyautogui.press('r')
-    pyautogui.hotkey('ctrl', 'c')
-    planilha_em_foco = pyperclip.paste()
-    pyautogui.press('esc')
-
-    return planilha_em_foco.strip()
-
-# Verifica se a planilha correta está em foco
-def validar_planilha(padrao):
-    aba_excel = copiar_foco()
-    if aba_excel == padrao:
-        pyperclip.copy('')
-        return True
-    else:
-        pyperclip.copy('')
-        messagebox.showerror('Erro', 'Planilha não encontrada ou fora de foco')
-        return False
-
-def criar_pasta(nome_pasta):
-    if not os.path.exists(nome_pasta):
-        os.makedirs(nome_pasta)
-
-def apagar_arquivo():
-    nome_arquivo = selecionar_arquivo()
-
-    if not nome_arquivo:
-        return
-
-    if os.path.exists(nome_arquivo):
-        os.remove(nome_arquivo)
-        messagebox.showinfo('Sucesso', 'O arquivo foi removido com sucesso!')
-
-def selecionar_arquivo():
-    arquivo = filedialog.askopenfilename(
-        title='Selecione o arquivo',
-        initialdir='data/notas_fiscais',
-        filetypes=[('Arquivos CSV', '*.csv'), ('Todos os arquivos', '*.*')]
-    )
-    return arquivo
 
 def salvar_arquivo():
     caminho = filedialog.asksaveasfilename(
