6f031e024028e0aeb0cebd99f88517ec2e665d3c
devlucaass <lucasvinicius3719@gmail.com>
2026-06-07 15:49:39 -0300

    refactor: remoção de códigos duplicados e melhoria no fluxo do programa

--- src/ui/application.py
+++ src/ui/application.py
@@ -11,7 +11,6 @@ from utils.funcs import *
 set_appearance_mode('dark')
 
 class Application:
-    # Inicializa a aplicação, criando a janela principal, o frame e os widgets (labels, entrys, buttons e radio buttons).
     def __init__(self):
         self.root = CTk()
         self.config = load_config()
@@ -19,7 +18,6 @@ class Application:
         self.frame()
         self.widgets()
     
-    # Configura a janela principal da aplicação, definindo o título, o ícone, o tamanho e a possibilidade de redimensionamento.
     def window(self):
         self.root.title('Leitor de Notas Fiscais')
         self.root.iconbitmap('assets/icons/logo.ico')
@@ -30,7 +28,6 @@ class Application:
         self.frame_principal = CTkFrame(master=self.root, fg_color='#32a14f', border_color="#75d58f", border_width=3)
         self.frame_principal.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
 
-    # Cria os widgets da aplicação, incluindo labels, entrys, buttons e radio buttons, e os posiciona na janela utilizando o método place() para definir a posição relativa (relx e rely) e o tamanho (width e height) de cada widget.
     def widgets(self):
         # Imagens
         img_qrcode = Image.open('assets/images/qrcode.png')
@@ -64,90 +61,84 @@ class Application:
         self.lb_erro_chave.place(relx=0.20, rely=0.418)
 
         # Buttons
-        self.btn_iniciar = CTkButton(master=self.frame_principal, text='Iniciar', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=self.iniciar_modo_sem_qrcode)
-        self.btn_iniciar.place(relx=0.242, rely=0.500
-        )
+        self.btn_iniciar = CTkButton(master=self.frame_principal, text='Iniciar', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=lambda: self.iniciar(False))
+        self.btn_iniciar.place(relx=0.242, rely=0.500)
 
-        self.btn_ler_qrcode = CTkButton(master=self.frame_principal, text='Ler QRCode', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_qrcode, compound='left', command=self.iniciar_modo_qrcode)
+        self.btn_ler_qrcode = CTkButton(master=self.frame_principal, text='Ler QRCode', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_qrcode, compound='left', command=lambda: self.iniciar(True))
         self.btn_ler_qrcode.place(relx=0.242, rely=0.600)
 
         self.btn_gerar_modelo_planiilha = CTkButton(master=self.frame_principal, text='Gerar modelo de planilha', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=gerar_modelo_planilha)
         self.btn_gerar_modelo_planiilha.place(relx=0.242, rely=0.700)
 
-    # Limpa as mensagens de erro exibidas na interface, definindo o texto dos labels de erro como vazio e garantindo que a interface fique limpa para novas validações.
     def limpar_erros(self):
         self.lb_erro_celula.configure(text='')
         self.lb_erro_chave.configure(text='')
 
     def validacoes(self, usar_qrcode=False):
         self.limpar_erros()
-        self.celula = self.entry_celula.get()
-        self.chave = self.entry_chave.get()
+
+        celula = self.entry_celula.get()
+        chave = self.entry_chave.get()
 
         valido = True
 
-        if not valida_celula(self.celula):
+        if not valida_celula(celula):
             self.lb_erro_celula.configure(text='Célula inválida. Informe uma célula válida (ex: A1, B2, C3...).')
             valido = False
 
-        if not usar_qrcode and not self.chave:
+        if not usar_qrcode and not chave:
             self.lb_erro_chave.configure(text='Informe uma chave válida ou use o modo QRCode.')
             valido = False
 
         return valido
-    
-    # Carrega um arquivo CSV selecionado pelo usuário, extrai a chave da nota fiscal a partir do nome do arquivo e insere essa chave no campo de entrada correspondente na interface gráfica. Essa função é útil para facilitar o processo de carregamento de dados, permitindo que o usuário selecione um arquivo já existente em vez de digitar a chave manualmente.
-    def carregar_arquivo(self):
-        nome_arquivo = selecionar_arquivo()
 
-        if not nome_arquivo:
+    def iniciar(self, usar_qrcode=False):
+
+        if not self.validacoes(usar_qrcode):
             return
-        
-        nome_arquivo = os.path.basename(nome_arquivo)
-        chave = nome_arquivo.replace('nf_', '').replace('.csv', '')
 
-        self.entry_chave.delete(0, 'end')
-        self.entry_chave.insert(0, chave)
-        
-    def modo_qrcode(self):
-        self.limpar_erros()
+        self.iniciar_thread(lambda: self.executar(usar_qrcode))
 
-        qrcode = ler_qrcode()
+    def iniciar_thread(self, target):
+        threading.Thread(target=target,daemon=True).start()
 
-        if qrcode:
-            self.celula = self.entry_celula.get()
-            self.chave = qrcode.split('p=')[-1].split('|')[0]
+    def executar(self, usar_qrcode=False):
 
-            self.main(url=qrcode)
+        celula = self.entry_celula.get()
 
-    def modo_sem_qrcode(self):
-            self.main()
+        if usar_qrcode:
+            qrcode = ler_qrcode()
 
-    def iniciar_modo_qrcode(self):
-        if self.validacoes(usar_qrcode=True):
-            threading.Thread(target=self.modo_qrcode, daemon=True).start()
+            if not qrcode:
+                return
 
-    def iniciar_modo_sem_qrcode(self):
-        if self.validacoes():
-            threading.Thread(target=self.modo_sem_qrcode, daemon=True).start()
+            chave = qrcode.split('p=')[-1].split('|')[0]
+            self.fluxo_principal(chave=chave, celula=celula, url=qrcode)
 
-    # Fluxo principal da aplicação, que é executado quando o usuário clica no botão "Iniciar". Dependendo do modo selecionado (coletar dados, inserir dados ou ambos), a função executa as ações correspondentes, como configurar o navegador, coletar os dados da nota fiscal e inserir os dados no Excel. O fluxo é executado em uma thread separada para evitar que a interface gráfica fique congelada durante a execução das tarefas.
-    def main(self, url=None):
-            messagebox.showinfo('Iniciando', 'Iniciando automação...')
-            sefaz = SefazBot(self.chave)
+        else:
+            chave = self.entry_chave.get()
+            self.fluxo_principal(chave=chave, celula=celula)
 
-            if url:
-                sefaz.configura_navegador(url, headless=True)
-            else:
-                sefaz.configura_navegador('https://nfce.sefaz.pe.gov.br/nfce/consulta', headless=False)
+    def fluxo_principal(self, chave, celula, url=None):
 
-            if not url:
-                sefaz.digita_chave()
+        messagebox.showinfo('Iniciando', 'Iniciando automação...')
 
-            dados = sefaz.coleta_dados()
+        sefaz = SefazBot(chave)
 
-            excel = ExcelBot(dados)
-            excel.inserir_no_excel(self.config['caminho_excel'], self.celula)
+        if url:
+            sefaz.configura_navegador(url, headless=True)
+
+        else:
+            sefaz.configura_navegador(self.config['url_site'], headless=False)
+            sefaz.digita_chave()
+
+        dados = sefaz.coleta_dados()
+
+        if not dados:
+            return
+
+        excel = ExcelBot(dados)
+        excel.inserir_no_excel(celula)
 
     def run(self):
         self.root.mainloop()
