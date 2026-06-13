62d024522d999bd227bd51dec90d336873831cc6
devlucaass <lucasvinicius3719@gmail.com>
2026-06-13 13:12:24 -0300

    feat: implementa tratamento de erro com mensagem

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -1,5 +1,14 @@
 import json
+from tkinter import messagebox
 
 def load_config():
-    with open('src/config/settings.json', 'r', encoding='utf-8') as file:
-        return json.load(file)  
+    try:
+        with open('src/config/settings.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+        
+    except Exception:
+        messagebox.showerror('Erro', 'Arquivo de configuração com erros. Verifique e tente novamente.')
+        return {}
+      
+    
+    
