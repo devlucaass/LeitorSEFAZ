b902ea60d7febcaf9310b228174da6c2f4c754ac
devlucaass <lucasvinicius3719@gmail.com>
2026-06-24 18:56:34 -0300

    refactor: adiciona o caminho do arquivo .json no arquivo

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -1,14 +1,23 @@
 import json
 from tkinter import messagebox
 
+CAMINHO_CONFIG = 'src/config/settings.json'
+
 def load_config():
     try:
-        with open('src/config/settings.json', 'r', encoding='utf-8') as file:
+        with open(CAMINHO_CONFIG, 'r', encoding='utf-8') as file:
             return json.load(file)
         
     except Exception:
         messagebox.showerror('Erro', 'Arquivo de configuração com erros. Verifique e tente novamente.')
         return {}
-      
     
+def save_config(caminho_excel, nome_planilha):
+    config = {
+        'caminho_excel': caminho_excel,
+        'nome_planilha': nome_planilha
+    }
+    
+    with open(CAMINHO_CONFIG, 'w', encoding='utf-8') as file:
+        json.dump(config, file, indent=4, ensure_ascii=False)
     
