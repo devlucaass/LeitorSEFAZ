e0d04e724549e6032cd23ebc41633c66172ffac3
devlucaass <lucasvinicius3719@gmail.com>
2026-08-18 19:47:21 -0300

    refactor: separa widgets por categoria e modifica o nome das variáveis e métodos

--- src/ui/application.py
+++ src/ui/application.py
@@ -1,26 +1,22 @@
 import threading
 
-from PIL import Image
-from tkinter import messagebox
 from customtkinter import (
-    set_appearance_mode,
     CTk,
-    CTkFrame,
-    CTkLabel,
-    CTkEntry,
     CTkButton,
-    CTkImage
+    CTkEntry,
+    CTkFrame,
+    CTkImage,
+    CTkLabel,
+    set_appearance_mode,
 )
+from PIL import Image
 
-from services.sefaz_client import SefazClient
+from config.config_manager import load_config
 from services.excel_bot import ExcelBot
-from utils.validators import valida_celula
+from services.sefaz_client import SefazClient
 from ui.settings_window import SettingsWindow
-
-from utils.funcs import (
-    gerar_modelo_planilha,
-    ler_qrcode
-)
+from utils.funcs import gerar_modelo_planilha, read_qrcode
+from utils.validators import valida_celula
 
 set_appearance_mode('dark')
 
@@ -28,146 +24,147 @@ class Application:
     def __init__(self):
         self.root = CTk()
         self.window()
-        self.frame()
-        self.widgets()
-    
+        self.frames()
+        self.images()
+        self.labels()
+        self.entries()
+        self.buttons()
+
     def window(self):
         self.root.title('Leitor de Notas Fiscais')
         self.root.iconbitmap('assets/icons/logo.ico')
         self.root.geometry('500x500')
         self.root.resizable(False, False)
 
-    def frame(self):
-        self.frame_principal = CTkFrame(master=self.root, fg_color='#32a14f', border_color="#75d58f", border_width=3)
-        self.frame_principal.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
+    def frames(self):
+        self.main_frame = CTkFrame(master=self.root, fg_color='#32a14f', border_color="#75d58f", border_width=3)
+        self.main_frame.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
 
-    def widgets(self):
-        # Imagens
+    def images(self):
         img_qrcode = Image.open('assets/images/qrcode.png')
         img_gear = Image.open('assets/images/gear.png')
 
         self.img_qrcode = CTkImage(light_image=img_qrcode, dark_image=img_qrcode, size=(35,35))
         self.img_gear = CTkImage(light_image=img_gear, dark_image=img_gear, size=(20,20))
 
-        # Labels
-        self.lb_author = CTkLabel(master=self.frame_principal, text='Desenvolvido por Lucas Vinícius', font=('Arial', 12, 'bold'))
+    def labels(self):
+        self.lb_author = CTkLabel(master=self.main_frame, text='Desenvolvido por Lucas Vinícius', font=('Arial', 12, 'bold'))
         self.lb_author.place(relx=0.293, rely=0.935)
 
-        self.lb_title = CTkLabel(master=self.frame_principal, text='SefazBot - Leitor de Notas', font=('Arial', 24, 'bold'))
+        self.lb_title = CTkLabel(master=self.main_frame, text='SefazBot - Leitor de Notas', font=('Arial', 24, 'bold'))
         self.lb_title.place(relx=0.195, rely=0.015)
 
-        self.lb_excel_cell = CTkLabel(master=self.frame_principal, text='Célula (do Excel)', font=('Arial', 18, 'bold'), text_color='white')
+        self.lb_excel_cell = CTkLabel(master=self.main_frame, text='Célula (do Excel)', font=('Arial', 18, 'bold'), text_color='white')
         self.lb_excel_cell.place(relx=0.335, rely=0.12)
 
-        self.lb_access_key = CTkLabel(master=self.frame_principal, text='Chave da nota', font=('Arial', 18, 'bold'), text_color='white')
+        self.lb_access_key = CTkLabel(master=self.main_frame, text='Chave da nota', font=('Arial', 18, 'bold'), text_color='white')
         self.lb_access_key.place(relx=0.352, rely=0.300)
 
-        # Entries
-        self.entry_excel_cell = CTkEntry(master=self.frame_principal, placeholder_text='Informe a célula do Excel que deseja começar...', justify='center', width=300, fg_color='#235D34', border_color='#235D34')
-        self.entry_excel_cell.place(relx=0.18, rely=0.18)
-
-        self.entry_access_key = CTkEntry(master=self.frame_principal, placeholder_text='Informe a chave da nota fiscal...', justify='center', width=300, fg_color='#235D34', border_color='#235D34')
-        self.entry_access_key.place(relx=0.18, rely=0.360)
-
-        # Labels de erro
-        self.lb_error_excel_cell = CTkLabel(master=self.frame_principal, text='', font=('Arial', 12, 'bold'), text_color='red')
+        self.lb_error_excel_cell = CTkLabel(master=self.main_frame, text='', font=('Arial', 12, 'bold'), text_color='red')
         self.lb_error_excel_cell.place(relx=0.15, rely=0.237)
 
-        self.lb_error_access_key = CTkLabel(master=self.frame_principal, text='', font=('Arial', 12, 'bold'), text_color='red')
+        self.lb_error_access_key = CTkLabel(master=self.main_frame, text='', font=('Arial', 12, 'bold'), text_color='red')
         self.lb_error_access_key.place(relx=0.20, rely=0.418)
 
-        # Buttons
-        self.btn_start = CTkButton(master=self.frame_principal, text='Iniciar', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=lambda: self.start(False))
+    def entries(self):
+        self.entry_excel_cell = CTkEntry(master=self.main_frame, placeholder_text='Informe a célula do Excel que deseja começar...', justify='center', width=300, fg_color='#235D34', border_color='#235D34')
+        self.entry_excel_cell.place(relx=0.18, rely=0.18)
+
+        self.entry_access_key = CTkEntry(master=self.main_frame, placeholder_text='Informe a chave da nota fiscal...', justify='center', width=300, fg_color='#235D34', border_color='#235D34')
+        self.entry_access_key.place(relx=0.18, rely=0.360)
+
+    def buttons(self):
+        self.btn_start = CTkButton(master=self.main_frame, text='Iniciar', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=lambda: self.start(False))
         self.btn_start.place(relx=0.242, rely=0.500)
 
-        self.btn_read_qrcode = CTkButton(master=self.frame_principal, text='Ler QRCode', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_qrcode, compound='left', command=lambda: self.start(True))
+        self.btn_read_qrcode = CTkButton(master=self.main_frame, text='Ler QRCode', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_qrcode, compound='left', command=lambda: self.start(True))
         self.btn_read_qrcode.place(relx=0.242, rely=0.600)
 
-        self.btn_generate_spreadsheet_template = CTkButton(master=self.frame_principal, text='Gerar modelo de planilha', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=gerar_modelo_planilha)
+        self.btn_generate_spreadsheet_template = CTkButton(master=self.main_frame, text='Gerar modelo de planilha', font=('Arial', 15, 'bold'), width=239, height=35, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", command=gerar_modelo_planilha)
         self.btn_generate_spreadsheet_template.place(relx=0.242, rely=0.700)
 
-        self.btn_settings = CTkButton(master=self.frame_principal, text='', font=('Arial', 15, 'bold'), width=30, height=30, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_gear, compound='left', command=self.open_window_settings)
+        self.btn_settings = CTkButton(master=self.main_frame, text='', font=('Arial', 15, 'bold'), width=30, height=30, fg_color='#235D34', border_color="#72A782", border_width=2, hover_color="#33844B", image=self.img_gear, compound='left', command=self._open_window_settings)
         self.btn_settings.place(relx=0.910, rely=0.920)
 
-    def open_window_settings(self):
+    def _open_window_settings(self):
         self.root.withdraw()
 
         self.settings_window = SettingsWindow(self.root)
-        self.settings_window.protocol("WM_DELETE_WINDOW", self.close_window_settings)
+        self.settings_window.protocol("WM_DELETE_WINDOW", self._close_window_settings)
 
-    def close_window_settings(self):
+    def _close_window_settings(self):
         self.settings_window.destroy()
         self.root.deiconify()
 
-    def clear_errors_messages(self):
+    def _clear_errors_messages(self):
         self.lb_error_excel_cell.configure(text='')
         self.lb_erro_chave.configure(text='')
 
-    def validations(self, usar_qrcode=False):
-        self.clear_errors_messages()
+    def validations(self, use_qrcode=False):
+        self._clear_errors_messages()
 
-        excel_cell = self.entry_celula.get()
+        excel_cell = self.entry_excel_cell.get()
         access_key = self.entry_access_key.get()
 
-        valido = True
+        is_valid = True
 
         if not valida_celula(excel_cell):
             self.lb_error_excel_cell.configure(text='Célula inválida. Informe uma célula válida (ex: A1, B2, C3...).')
-            valido = False
+            is_valid = False
 
-        if not usar_qrcode and not access_key:
+        if not use_qrcode and not access_key:
             self.lb_erro_chave.configure(text='Informe uma chave válida ou use o modo QRCode.')
-            valido = False
+            is_valid = False
 
-        return valido
+        return is_valid
 
-    def start(self, usar_qrcode=False):
+    def start(self, use_qrcode=False):
 
-        if not self.validations(usar_qrcode):
+        if not self.validations(use_qrcode):
             return
 
-        self.start_thread(lambda: self.run(usar_qrcode))
+        self.start_thread(lambda: self.run_app(use_qrcode))
 
     def start_thread(self, target):
         threading.Thread(target=target,daemon=True).start()
 
-    def run(self, usar_qrcode=False):
+    def run_app(self, use_qrcode=False):
 
-        excel_cell = self.entry_celula.get()
+        excel_cell = self.entry_excel_cell.get()
 
-        if usar_qrcode:
-            qrcode = ler_qrcode()
+        if use_qrcode:
+            qrcode = read_qrcode()
 
             if not qrcode:
                 return
 
-            chave = qrcode.split('p=')[-1].split('|')[0]
-            self.fluxo_principal(chave=chave, celula=excel_cell, url=qrcode)
+            access_key = qrcode.split('p=')[-1].split('|')[0]
+            self.main(access_key=access_key, excel_cell=excel_cell, url=qrcode)
 
         else:
-            chave = self.entry_access_key.get()
-            self.fluxo_principal(chave=chave, celula=excel_cell)
+            access_key = self.entry_access_key.get()
+            self.main(access_key=access_key, excel_cell=excel_cell)
 
-    def fluxo_principal(self, chave, celula, url=None):
+    def main(self, access_key, excel_cell, url=None):
 
-        messagebox.showinfo('Iniciando', 'Iniciando automação...')
+        print("Iniciando automação...")
 
-        sefaz = SefazClient(chave)
+        sefaz = SefazClient(access_key)
 
         if url:
-            sefaz.configura_navegador(url, headless=True)
+            sefaz.configure_browser(url, headless=True)
 
         else:
-            sefaz.configura_navegador(URL_SEFAZ, headless=False)
-            sefaz.digita_chave()
+            sefaz.configure_browser(load_config('sefaz_url'), headless=False)
+            sefaz.enter_access_key()
 
-        dados = sefaz.coleta_dados()
+        data = sefaz.collect_data()
 
-        if not dados:
+        if not data:
             return
 
-        excel = ExcelBot(dados)
-        excel.inserir_no_excel(celula)
+        excel = ExcelBot(data)
+        excel.insert_data(excel_cell)
 
     def run(self):
         self.root.mainloop()
