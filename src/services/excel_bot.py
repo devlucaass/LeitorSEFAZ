54c90d0fdbde31f57a0f6a12fa5b646a7bf3b52c
devlucaass <lucasvinicius3719@gmail.com>
2026-08-18 18:32:48 -0300

    refactor: organiza código e identação

--- src/services/excel_bot.py
+++ src/services/excel_bot.py
@@ -1,12 +1,13 @@
 import xlwings as xw
+
 from config.config_manager import load_config
 
-class ExcelBot:
 
+class ExcelBot:
     def __init__(self, data):
         self.data = data
         self.config = load_config()
-        self.excel_path = self.config['excel_path']
+        self.excel_path = self.config["excel_path"]
 
     def insert_data(self, start_cell):
         try:
@@ -17,30 +18,28 @@ class ExcelBot:
             ws.range(start_cell).value = excel_values
 
             wb.save()
-            
-        except Exception as e:
+
+        except FileNotFoundError as e:
             print(f"Erro ao inserir dados no Excel: {e}")
 
     def _open_workbook(self):
         return xw.Book(self.excel_path)
 
     def _get_worksheet(self, wb):
-        sheet_name = self.config['sheet_name']
+        sheet_name = self.config["sheet_name"]
         return wb.sheets[sheet_name]
 
     def _prepare_excel_values(self):
         return [
             [
-                row['date'],
-                row['establishment_name'],
-                row['product'],
-                row['unit'],
-                row['purchased_quantity'],
-                row['unit_price'],
-                row['quantity'],
-                row['total_price']
+                row["date"],
+                row["establishment_name"],
+                row["product"],
+                row["unit"],
+                row["purchased_quantity"],
+                row["unit_price"],
+                row["quantity"],
+                row["total_price"],
             ]
             for row in self.data
         ]
-
-
