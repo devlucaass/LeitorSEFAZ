ad3ec201d886dc062693034342cb730b883dcc38
devlucaass <lucasvinicius3719@gmail.com>
2026-08-25 15:10:52 -0300

    feat: cria uma classe QRCode para abrigar os métodos relacionados

--- src/utils/qrcode.py
+++ src/utils/qrcode.py
@@ -4,35 +4,47 @@ import pyzbar
 from utils.validators import Validators
 
 
-def read_qrcode():
-    cam = cv2.VideoCapture(0)
+class QRCode:
+    def read_qrcode(self):
+        camera = cv2.VideoCapture(0)
 
-    try:
-        while True:
-            result, frame = cam.read()
+        try:
+            while True:
+                result, frame = camera.read()
 
-            if not result:
-                break
+                if not result:
+                    break
 
-            qrcode = pyzbar.decode(frame)
+                qrcode = pyzbar.decode(frame)
 
-            for qr in qrcode:
-                url = qr.data.decode('utf-8')
-                access_key = url.split('p=')[-1].split('|')[0]
+                for qr in qrcode:
+                    url = qr.data.decode('utf-8')
+                    access_key = QRCode._extract_access_key()
 
-                if Validators.validate_access_key(access_key):
-                    return url
+                    if Validators.validate_access_key(access_key):
+                        return url
 
-                print(f'QR Code inválido: {url}')
-                return False
+                    print(f'QR Code inválido: {url}')
+                    return False
 
-            cv2.imshow('Leitor de QRCode', frame)
+                cv2.imshow('Leitor de QRCode', frame)
 
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
+                
+                if cv2.waitKey(1) & 0xFF == 27:
+                    break
 
-        return False
+            return False
 
-    finally:
-        cam.release()
+        finally:
+            QRCode._close_camera()
+
+
+    @staticmethod
+    def _extract_access_key(value):
+        return value.split('p=')[-1].split('|')[0]
+
+    @staticmethod
+    def _close_camera(camera):
+        camera.release()
         cv2.destroyAllWindows()
+
