b5f60e30136caa80e510cd47daab68c1bdfe96e0
devlucaass <lucasvinicius3719@gmail.com>
2026-08-26 15:02:53 -0300

    feat: adiciona os paths dos recursos visuais como constantes

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,1 +1,7 @@
+# URLs
 URL_SEFAZ = "https://nfce.sefaz.pe.gov.br/nfce/consulta"
+
+# ASSETS
+APP_ICON_PATH = "assets/icons/logo.ico"
+QRCODE_IMAGE_PATH = "assets/images/qrcode.png"
+GEAR_IMAGE_PATH = "assets/images/gear.png"
