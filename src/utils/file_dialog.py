3d973f067cdc4021d9d63af987453d2690a804a1
devlucaass <lucasvinicius3719@gmail.com>
2026-08-26 14:24:04 -0300

    refactor: muda o nome do arquivo de funcs.py para file_dialog.py e organiza o fluxo

---
+++ src/utils/file_dialog.py
@@ -0,0 +1,31 @@
+from tkinter import filedialog
+
+
+def select_file():
+    file_path = filedialog.askopenfilename(
+        title='Selecione uma planilha',
+        filetypes=[('Arquivos Excel', '*.xlsx')]
+    )
+
+    if not file_path:
+        return None
+
+    return file_path
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(
+        initialfile='spreadsheet_template.xlsx',
+        defaultextension='.xlsx',
+        filetypes=[('Arquivos Excel', '*.xlsx *.xlsm'), ('Todos os arquivos', '*.*')],
+        title='Salvar modelo da planilha'
+    )
+
+    if not file_path:
+        return
+    
+    return file_path
+
+
+
+
+
