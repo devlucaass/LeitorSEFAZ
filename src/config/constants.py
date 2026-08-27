f344834c19552f76d4f70a02660cf3ef276cb1f3
devlucaass <lucasvinicius3719@gmail.com>
2026-08-27 16:25:06 -0300

    feat: adiciona KEYS a relação de constantes

--- src/config/constants.py
+++ src/config/constants.py
@@ -5,3 +5,7 @@ URL_SEFAZ = "https://nfce.sefaz.pe.gov.br/nfce/consulta"
 APP_ICON_PATH = "assets/icons/logo.ico"
 QRCODE_IMAGE_PATH = "assets/images/qrcode.png"
 GEAR_IMAGE_PATH = "assets/images/gear.png"
+
+# KEYS
+EXCEL_PATH_KEY = "excel_path"
+SHEET_NAME_KEY = "sheet_name"
