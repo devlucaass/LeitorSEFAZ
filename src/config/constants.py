9430c4f3d65769643305f336c87c5904d26431d4
devlucaass <lucasvinicius3719@gmail.com>
2026-09-01 14:07:01 -0300

    fix: resolve o problema dos caminhos relativos

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,10 +1,12 @@
+from utils.resource_path import resource_path
+
 # URLs
 URL_SEFAZ = "https://nfce.sefaz.pe.gov.br/nfce/consulta"
 
 # ASSETS
-APP_ICON_PATH = "assets/icons/logo.ico"
-QRCODE_IMAGE_PATH = "assets/images/qrcode.png"
-GEAR_IMAGE_PATH = "assets/images/gear.png"
+APP_ICON_PATH = resource_path("assets/icons/logo.ico")
+QRCODE_IMAGE_PATH = resource_path("assets/images/qrcode.png")
+GEAR_IMAGE_PATH = resource_path("assets/images/gear.png")
 
 # KEYS
 EXCEL_PATH_KEY = "excel_path"
